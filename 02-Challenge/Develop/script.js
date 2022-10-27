830e15761c6d6f63e5887cdcdb86d9f37b4009be
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-10-27 14:11:46 +0100

    updated timer to changes per s

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,8 +1,9 @@
 var times = {
-    startHour: 9,
-    endHour: 17,
-}
+  startHour: 9,
+  endHour: 17,
+};
 
+getHeaderDate();
 
 // <div class="row">
 // <div class="col-sm-2 hour">9AM</div>
@@ -10,31 +11,48 @@ var times = {
 //   <textarea class="col-md-10 description"></textarea>
 // </div>
 // <div class="col-sm-2">
-//   <button class="btm btn-primart saveBtn">Save</button>
+//   <button class="btn btn-primary saveBtn">Save</button>
 // </div>
 // </div>
 
-//TODOfunction to generate the time slots 
+//TODOfunction to generate the time slots
 //FOR LOOP
 function createTimeSlots() {
 
+    //load the saved data from local storage 
 
+//create for loop to go through and create all elements for the UI
+  for (hour = times.startHour; hour <= times.endHour; hour++) {
+    var timeSlot = $('<div>').addClass("row");
+
+    var time = $("<div>").addClass('col-sm-2 hour').text(moment(hour, 'h'));
+    var desc = $('<textarea>').addClass('col-md-10 description');
+    var text = $('<button>').addClass('btn btn-primary saveBtn');
+  }
+
+  //add event hsndler to the save button so it saves task to local storage 
+
+ //append them all to the HTML
 
 
 }
 
-//TODO function to update the color of the time slots 
+//TODO function to update the color of the time slots
 // the classs changes depending on the hour which is realted to a different CSS style IF STATEMENTS
 
-for (hour=times.startHour; hour <= times.endHour; hour++) {
-    var time = "";
-    var desc =  "" ;
-    var text = "";
-
+//using moment to get current date at top
+function getHeaderDate() {
+  var today = moment();
+  $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
 }
 
-//using moment to get current date at top 
-var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY, h:mm"));
+//allows time to change by second 
+setInterval(function() {
+    getHeaderDate();
 
-//TODO function to save the information entered to local storage 
+},1000)
+
+//TODO function to save the information entered to local storage
+function savePlan() {
+
+}
