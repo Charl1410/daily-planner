7ecc76a4833cdd080f643cd57746bf55727aca7c
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-10-29 14:34:25 +0100

    working on color changes for slots

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -5,73 +5,97 @@ var times = {
 
 //TODO function to save the information entered to local storage
 function savePlan() {
+  //     //need to tell it where to get the imformation from (event bubbling)
+  // console.log('hello')
+  // var hour = " ";
+  // var textEntry = $(document).$('.description').val();
+  // localStorage.setItem(hour, textEntry);
+  //   console.log(textEntry);
+}
 
-    //need to tell it where to get the imformation from (event bubbling)
+//calls function to impliment the time into page
+
+//TODO create function to check time and change the css of the boxes past present future 
+
+function updateColor() {
+
+var currentHour = moment().hour();
+
+console.log(currentHour);
 
 
-    localStorage.setItem(hour, task);
+$(".row").each(function () {
+  var hour = $(this).attr("data-hour", hour);
+
+  if (hour < currentHour) {
+    //add class ".past"
+    $(this).find(".description").addClass("past");
+    //if its equal to the current hour
+  } else if (hour == currentHour) {
+    //add class ".present"
+    $(this).find(".description").addClass("present");
+    //if its over the current hour
+  } else {
+    //add class ".future"
+    $(this).find(".description").addClass("future");
+  }
+});
+
 }
 
 
 
-
-//calls function to impliment the time into page 
-getHeaderDate();
 
 
 
 //TODOfunction to generate the time slots
 function createTimeSlots() {
+  // 1. load the saved data from local storage
 
-    // 1. load the saved data from local storage 
-
-// 2. create for loop to go through and create all elements for the UI
+  //create for loop to go through and create all elements for the UI
   for (hour = times.startHour; hour <= times.endHour; hour++) {
-    
-    var timeSlot = $('<div>').addClass("row"); //main inner container
-    var timeHour = $("<div>").addClass('col-sm-2 hour').text(moment(hour, 'h').format('h A'));
-    var outerBox = $('<div>').addClass('col-sm-8 row past');
-    var enterText = $('<textarea>').addClass('col-md-10 description');
-    var buttonContainer = $('<div>').addClass('col-sm-2');
-    var button = $('<button>').addClass('btn btn-primary saveBtn');
+    var timeSlot = $("<div>").addClass("row"); //main inner container
+    timeSlot.attr("data-hour", hour);
+    var timeHour = $("<div>")
+      .addClass("col-sm-2 hour")
+      .text(moment(hour, "h").format("h A"));
+    var outerBox = $("<div>").addClass("col-sm-8 row past");
+    var enterText = $("<textarea>").addClass("col-md-10 description");
+    var buttonContainer = $("<div>").addClass(
+      "col-sm-2 btn btn-primary saveBtn d-flex align-items-center justify-content-center"
+    );
+    buttonContainer.on("click", savePlan);
+    var button = $("<button>").addClass("fas fa-save");
 
     outerBox.append(enterText);
     buttonContainer.append(button);
     timeSlot.append(timeHour);
+    timeSlot.append(buttonContainer);
+    timeSlot.append(outerBox);
 
-    $('.container').append(timeSlot);
-
-    console.log(timeSlot)
-
-
+    $(".container").append(timeSlot);
   }
 
-  //add event handler to the save button so it saves task to local storage 
+  //add event handler to the save button so it saves task to local storage
 
- //append them all to the HTML
-
-
+  //append them all to the HTML
 }
 
-createTimeSlots()
+createTimeSlots();
 
 //TODO function to update the color of the time slots
-// the classs changes depending on the hour which is realted to a different CSS style IF STATEMENTS
-
+updateColor()
 //using moment to get current date at top
 
-//TODO create function to impliment the time at the top of page 
+//TODO create function to impliment the time at the top of page
 function getHeaderDate() {
   var today = moment();
   $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
-
 }
 
-//allows time to change by second 
-setInterval(function() {
-    getHeaderDate();
+//allows time to change by second
+setInterval(function () {
+  getHeaderDate();
+}, 1000);
 
-},1000)
-
-
-
+getHeaderDate();
