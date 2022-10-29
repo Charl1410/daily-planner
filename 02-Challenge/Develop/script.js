3448a4ea2c4cf2e766931d44ccfc4ab592916125
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-10-29 19:44:21 +0100

    setting local storage fuction

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -4,16 +4,18 @@ var times = {
 };
 
 //TODO function to save the information entered to local storage
-function savePlan() {
-  //     //need to tell it where to get the imformation from (event bubbling)
-  // console.log('hello')
-  // var hour = " ";
-  // var textEntry = $(document).$('.description').val();
-  // localStorage.setItem(hour, textEntry);
-  //   console.log(textEntry);
+function savePlan(e) {
+  
+  var textEntry = $(e.target).find('.description').val(); //this isnt working 
+  var hour = $(e.target).parent().attr('data-hour');
+   
+  localStorage.setItem(hour, textEntry);
+  
+   console.log(textEntry)
+
+   console.log('saved')
 }
 
-//calls function to impliment the time into page
 
 //TODO create function to check time and change the css of the boxes past present future 
 
@@ -21,11 +23,13 @@ function updateColor() {
 
 var currentHour = moment().hour();
 
-console.log(currentHour);
+//console.log(currentHour);
 
 
 $(".row").each(function () {
-  var hour = $(this).attr("data-hour", hour);
+  var hour = $(this).attr("data-hour");
+
+  //console.log(hour) //something going wrong here - number + undefined 
 
   if (hour < currentHour) {
     //add class ".past"
@@ -44,10 +48,6 @@ $(".row").each(function () {
 }
 
 
-
-
-
-
 //TODOfunction to generate the time slots
 function createTimeSlots() {
   // 1. load the saved data from local storage
@@ -56,9 +56,7 @@ function createTimeSlots() {
   for (hour = times.startHour; hour <= times.endHour; hour++) {
     var timeSlot = $("<div>").addClass("row"); //main inner container
     timeSlot.attr("data-hour", hour);
-    var timeHour = $("<div>")
-      .addClass("col-sm-2 hour")
-      .text(moment(hour, "h").format("h A"));
+    var timeHour = $("<div>").addClass("col-sm-2 hour").text(moment(hour, "h").format("h A"));
     var outerBox = $("<div>").addClass("col-sm-8 row past");
     var enterText = $("<textarea>").addClass("col-md-10 description");
     var buttonContainer = $("<div>").addClass(
@@ -81,21 +79,26 @@ function createTimeSlots() {
   //append them all to the HTML
 }
 
-createTimeSlots();
+
 
 //TODO function to update the color of the time slots
-updateColor()
+
 //using moment to get current date at top
 
 //TODO create function to impliment the time at the top of page
-function getHeaderDate() {
+function init() {
   var today = moment();
   $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
+
 }
 
 //allows time to change by second
 setInterval(function () {
-  getHeaderDate();
+  init();
 }, 1000);
 
-getHeaderDate();
+init()
+
+createTimeSlots()
+
+updateColor()
