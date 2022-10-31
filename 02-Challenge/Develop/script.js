4d61f76205e83e865696ab2e4248ffe2e129b730
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2022-10-31 11:35:48 +0000

    updated readme, cleared local storage

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,66 +1,52 @@
 var times = {
   startHour: 9,
-  endHour: 22,
+  endHour: 17,
 };
 
-//TODO function to save the information entered to local storage
+//function to save the information entered to local storage
 function savePlan(e) {
-  
-  var textEntry = $(e.target).parent().find('.description').val(); //this isnt working 
-  var hour = $(e.target).parent().attr('data-hour');
-   
-  localStorage.setItem(hour, textEntry); 
-  
-   console.log(textEntry)
+  var textEntry = $(e.target).parent().find(".description").val(); 
+  var hour = $(e.target).parent().attr("data-hour");
 
-   console.log('saved')
+  //stores text entry to local storage under the key of specific hour 
+  localStorage.setItem(hour, textEntry);
+
 }
 
-
-//TODO create function to check time and change the css of the boxes past present future 
+//check time to update the css color by changing the class based on past present future 
 
 function updateColor() {
-
-  var currentHour = moment().hour(); 
-
-  //console.log(currentHour);
-
+  var currentHour = moment().hour();
 
   $(".time-block").each(function (index, row) {
     var hour = $(row).attr("data-hour");
 
-    //console.log(hour) //something going wrong here - number + undefined 
-
     if (hour < currentHour) {
-      //add class ".past"
       $(row).find(".description").addClass("past");
-      //if its equal to the current hour
     } else if (hour == currentHour) {
-      //add class ".present"
       $(row).find(".description").addClass("present");
-      //if its over the current hour
     } else {
-      //add class ".future"
       $(row).find(".description").addClass("future");
     }
   });
-
 }
-
 
 //TODOfunction to generate the time slots
 function createTimeSlots() {
-  // 1. load the saved data from local storage
 
   //create for loop to go through and create all elements for the UI
 
   for (var hour = times.startHour; hour <= times.endHour; hour++) {
-    var storedItem = localStorage.getItem(hour)
+    var storedItem = localStorage.getItem(hour);
     var timeSlot = $("<div>").addClass("row time-block"); //main inner container
     timeSlot.attr("data-hour", hour);
-    var timeHour = $("<div>").addClass("col-sm-2 hour").text(moment(hour, "h").format("h A"));
+    var timeHour = $("<div>")
+      .addClass("col-sm-2 hour")
+      .text(moment(hour, "h").format("h A"));
     var outerBox = $("<div>").addClass("col-sm-8 row");
-    var enterText = $("<textarea>").addClass("col-sm-12 description").val(storedItem);
+    var enterText = $("<textarea>")
+      .addClass("col-sm-12 description")
+      .val(storedItem);
     var buttonContainer = $("<div>").addClass(
       "col-sm-2 btn btn-primary saveBtn d-flex align-items-center justify-content-center"
     );
@@ -70,31 +56,17 @@ function createTimeSlots() {
     outerBox.append(enterText);
     buttonContainer.append(button);
     timeSlot.append(timeHour);
-    timeSlot.append(outerBox); 
+    timeSlot.append(outerBox);
     timeSlot.append(buttonContainer);
 
     $(".container").append(timeSlot);
-
-
-
   }
-
-  //add event handler to the save button so it saves task to local storage
-
-  //append them all to the HTML
 }
 
-
-
-//TODO function to update the color of the time slots
-
-//using moment to get current date at top
-
-//TODO create function to impliment the time at the top of page
+//impliment the time at the top of page
 function init() {
   var today = moment();
   $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
-
 }
 
 //allows time to change by second
@@ -102,8 +74,8 @@ setInterval(function () {
   init();
 }, 1000);
 
-init()
+init();
 
-createTimeSlots()
+createTimeSlots();
 
-updateColor()
+updateColor();
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # daily-planner
+
+## Overview 
+    This daily planner is designed to update and display the current date and time. Users can input task into speicifc hour slots and click the save button to commit the task to local storage. When the page is refreshed or closed and reopened the task will remain in place. The color of each time slot should update to represent the past, present and future hour(s).
+
+## Acceptance criteria 
+    ```md
+    GIVEN I am using a daily planner to create a schedule
+    WHEN I open the planner
+    THEN the current day is displayed at the top of the calendar
+    WHEN I scroll down
+    THEN I am presented with timeblocks for standard business hours
+    WHEN I view the timeblocks for that day
+    THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    WHEN I click into a timeblock
+    THEN I can enter an event
+    WHEN I click the save button for that timeblock
+    THEN the text for that event is saved in local storage
+    WHEN I refresh the page
+    THEN the saved events persist
+    ```
+
+## Screenshots 
+[]!
